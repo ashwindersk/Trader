08575bc9bd19b14f8572fadb28f5db7267507c8b
ak16625@my.bristol.ac.uk <ak16625@my.bristol.ac.uk>
2020-07-29 16:52:17 +0100

    MDNRNN was not accurate, normal LSTM test

---
+++ Bristol-Stock-Gym/Transition/RNN2.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+class LSTM(nn.Module):
+
+    def __init__(self, output_size, input_size, hidden_size, num_layers, seq_length, fc1_out):
+        super(LSTM, self).__init__()
+        
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True).to(self.device)
+        self.fc1 = nn.Linear(hidden_size, fc1_out).to(self.device)
+        
+        self.fc2 = nn.Linear(fc1_out, output_size).to(self.device)
+
+    def forward(self, x):
+        h_0 = Variable(torch.zeros(
+            self.num_layers, x.size(0), self.hidden_size)).to(self.device)
+        
+        c_0 = Variable(torch.zeros(
+            self.num_layers, x.size(0), self.hidden_size)).to(self.device)
+        
+        # Propagate input through LSTM
+        ula, (h_out, _) = self.lstm(x, (h_0, c_0))
+        h_out = h_out.view(-1, self.hidden_size)
+        
+        x = self.fc1(h_out)
+        x = self.fc2(x)
+        return x
--- Bristol-Stock-Gym/Transition/TransitionModel.py
+++ Bristol-Stock-Gym/Transition/TransitionModel.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
 
 
-from RNN import MDNRNN
+from RNN2 import LSTM
 import torch
 import numpy as np 
 from torch import nn
@@ -12,12 +13,17 @@ from torch.autograd import Variable
 from torch.nn.utils import clip_grad_norm_
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# In[2]:
+
+
 ################
 #  DATALOADING #
 ################
 
 
-
+# In[3]:
 
 
 
@@ -27,24 +33,24 @@ def sliding_windows(data,seq_length):
 
     for i in range(len(data)-seq_length-1):
         _x = data[i:(i+seq_length)]
-        _y = data[(i+1):(i+1)+seq_length]
+        _y = data[(i+seq_length):(i+1)+seq_length]
         x.append(_x)
         y.append(_y)
 
     return np.array(x),np.array(y)
 
 
+# In[4]:
 
 
+bsz = 200
 
-bsz = 16
-
-data = np.load('../Regression/latent-action.npy')
-
+data = np.load('latent-action.npy')
+print(data[:,23233,:])
 #Reshaping data and seperating training and test set
 sc = MinMaxScaler()
 data = sc.fit_transform(data.squeeze(axis = 0))
-
+print(data[23233,:])
 seq_length = 4
 x, y = sliding_windows(data, seq_length)
 
@@ -75,17 +81,19 @@ trainloader = torch.utils.data.DataLoader(trainingset, batch_size=bsz, shuffle=F
 
 
 
-num_epochs = 2000
-learning_rate = 1e-7
+# In[5]:
 
+
+
+learning_rate = 1e-7
 input_size = 18
 hidden_size = 256
 num_layers = 1
-seq_length = 4
 num_classes = 1
-gaussians = 5
+fc1_out = 128
+output_size = 18
 
-mdnrnn = MDNRNN(z_size = input_size, n_hidden = hidden_size, n_gaussians = gaussians, n_layers = num_layers).to(device)
+lstm = LSTM(output_size, input_size, hidden_size, num_layers, seq_length, fc1_out)
 
 
 def detach(states):
@@ -95,68 +103,108 @@ def detach(states):
 # In[ ]:
 
 
-optimizer = torch.optim.Adam(mdnrnn.parameters(), lr = learning_rate)
+lstm.train()
+test_hist = []
+criterion = torch.nn.MSELoss().to(device)    # mean-squared error for regression
+optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
 
-epochs = 500
+for epoch in range(500):
 
-for epoch in range(epochs):
-    # Set initial hidden and cell states
-    hidden = mdnrnn.init_hidden(bsz)
+        outputs = lstm(trainX).to(device)
 
-    # Get mini-batch inputs and targets
-    total_loss = 0
-    j = 0
-    for i, (inputs, targets) in enumerate(trainloader):
-        # Forward pass
-        if inputs.size(0) != bsz:
-            continue
-        hidden = detach(hidden)
-        (pi, mu, sigma), hidden = mdnrnn(inputs.to(device), hidden)
-
-        loss = mdnrnn.criterion(targets.to(device), pi, mu, sigma)
-        # Backward and optimize
-        mdnrnn.zero_grad()
-        total_loss += loss.item()
-
+        optimizer.zero_grad()
+    
+    # obtain the loss function
+        loss = criterion(outputs, trainY)
+    
         loss.backward()
-        clip_grad_norm_(mdnrnn.parameters(), 0.5)
+    
         optimizer.step()
-        j += 1
-    total_loss /=j
-    if epoch % 2 == 0:
-        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch, epochs, total_loss))
-        torch.save(mdnrnn.state_dict(), '../Models/transition-regression-new')
+        if testX is not None:
+            with torch.no_grad():
+                y_test_pred = lstm(testX).to(device)
+                test_loss = criterion(y_test_pred.float(), testY).to(device)
+                test_hist.append( test_loss.item())
+
+        print("Epoch: %d, train loss: %1.5f, test loss: %1.5f" % (epoch, loss.item(), test_hist[epoch]))
+
+        torch.save(lstm.state_dict(), '../Models/test-transition')
 
 
 
+# In[ ]:
 
-import matplotlib.pyplot as plt
+
+mdnrnn.load_state_dict(torch.load('../Models/transition-regression-test', map_location='cpu'))
 
 mdnrnn.eval()
 size = train_size+test_size
-train_predict = mdnrnn(dataX[size-1000:size])
-data_predict = train_predict.data.numpy()[0:1000]
-dataY_truth = dataY.data.numpy()[size-1000:size]
-data_predict = sc_y.inverse_transform(data_predict)
-dataY_truth = sc_y.inverse_transform(dataY_truth)
-print(data_predict[0:5])
+zero = np.random.randint(testX.size(0))
+
+one = np.random.randint(testX.size(1))
+
+
+x = testX[zero:zero+1, one:one+1, :]
+y = testX[zero:zero+1, one+1:one+2, :]
+print(x.shape)
+hidden = mdnrnn.init_hidden(1)
+(pi, mu, sigma), _ = mdnrnn(x, hidden)
+
+y_preds = [torch.normal(mu, sigma)[:, :, i, :] for i in range(5)]
+
+new = [tens.detach().numpy().reshape(1,18) for tens in y_preds]
+
+
+print(y.shape)
+data_predict = new[2]
+dataY_truth = y.flatten()
+print(data_predict)
 print("---------------------------------------")
-print(data_truth[0:5])
+print(dataY_truth)
 
 
+# In[ ]:
 
 
-torch.save(mdnrnn.state_dict(), '../Models/transition-regression')
+x = torch.Tensor([0.9825, 0.4345, 0.9836, 0.2939, 0.3526, 0.7257, 0.4492, 0.1332,
+          0.5326, 0.5326, 0.5326, 0.5349, 0.5337, 0.5326, 0.0012, 0.0000,
+          0.4744, 0.5000])
+x = x.reshape((1,1,18))
+(pi,mu,sigma), _ = mdnrnn(x,hidden)
+pi = pi.reshape((1,5,18))
+y_preds = [torch.normal(mu, sigma)[:, :, i, :] for i in range(5)]
+y_preds = [tens.reshape(1,18) for tens in y_preds]
+y_preds = torch.cat(y_preds, dim = 0)
+
+y_pred = pi * y_preds
+            
+
+y_pred = torch.sum(y_pred, dim = 1)
+
+            #print(y_pred)
+y_pred = y_pred.detach().numpy()
+y_pred = y_pred.reshape((1,18))
+y_pred = sc.inverse_transform(y_pred)
+
+print(y_pred)
 
 
+# In[ ]:
+
+
+torch.save(mdnrnn.state_dict(), '../Models/transition-regression-new2')
+
+
+# In[ ]:
 
 
 import pickle
-with open('sc_transition', 'wb') as f:
-    pickle.dump(sc_y, f)
+with open('sc', 'wb') as f:
+    pickle.dump(sc, f)
     
 
 
+# In[ ]:
 
 
 
