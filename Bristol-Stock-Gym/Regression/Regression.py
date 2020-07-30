45af0d33f4fcb019bc09a724bdcb7c00e999395c
Ashwinder Khurana <ak16625@bc4login3.bc4.acrc.priv>
2020-07-30 02:53:24 +0100

    midprice regression new

--- Bristol-Stock-Gym/Regression/Regression.py
+++ Bristol-Stock-Gym/Regression/Regression.py
@@ -10,25 +10,8 @@ import numpy as np
 from torch import nn
 from sklearn.preprocessing import MinMaxScaler
 from torch.autograd import Variable
-import signal
-import sys
-import pickle
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-cuda = torch.cuda.is_available()
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    torch.save(lstm.state_dict(), '../Models/state2-regression')
-    print("Saved model..")
-    with open('sc_midprice', 'wb') as f:
-        pickle.dump(sc_y, f)
-    with open('sc_latent', 'wb') as f:
-        pickle.dump(sc_x, f)    
-    print("Saved scalars")
-    sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-
-
 
 # In[2]:
 
@@ -42,13 +25,13 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 
-def sliding_windows(x_data,y_data,seq_length):
+def sliding_windows(data,seq_length):
     x = []
     y = []
 
-    for i in range(len(y_data)-seq_length-1):
-        _x = x_data[i:(i+seq_length)]
-        _y = y_data[i+seq_length]
+    for i in range(data.shape[1]-seq_length-1):
+        _x = data[:,i:(i+seq_length)]
+        _y = data[:,i+seq_length]
         x.append(_x)
         y.append(_y)
 
@@ -59,38 +42,39 @@ def sliding_windows(x_data,y_data,seq_length):
 
 
 
-y_data = np.load('midprices.npy')
-x_data = np.load('latent.npy')
-
+data = np.load('midprices-new.npy')
 #Reshaping data and seperating training and test set
-sc_x = MinMaxScaler()
-x_data = sc_x.fit_transform(x_data.squeeze(axis = 0))
+sc = MinMaxScaler()
+data = sc.fit_transform(data.reshape(-1,1))
+data = data.reshape((1,data.shape[0]))
+seq_length = 4
+x, y = sliding_windows(data, seq_length)
 
-sc_y = MinMaxScaler()
-y_data = sc_y.fit_transform(y_data.reshape(-1,1))
-
-
-
-
-seq_length = 2
-x, y = sliding_windows(x_data,y_data, seq_length)
-
-
-
-
-
-train_size = int(len(y) * 0.67)
-test_size = len(y) - train_size
+print(y.shape)
+bsz = 64
+size = y.shape[0]
+train_size = int(size * 0.67)
+test_size = size - train_size
 
 dataX = Variable(torch.Tensor(np.array(x)))
 dataY = Variable(torch.Tensor(np.array(y)))
+print(dataX.shape, dataY.shape)
 
-trainX = Variable(torch.Tensor(np.array(x[0:train_size]))).to(device)
-trainY = Variable(torch.Tensor(np.array(y[0:train_size]))).to(device)
+trainX = dataX[0:train_size,:,:]
+trainY = dataY[0:train_size,:]
 
 
-testX = Variable(torch.Tensor(np.array(x[train_size:len(x)]))).to(device)
-testY = Variable(torch.Tensor(np.array(y[train_size:len(y)]))).to(device)
+testX = dataX[train_size:size, :,:]
+testY = dataY[train_size:size,:]
+print(trainY.shape)
+print(trainX.shape)
+trainingset =[]
+for i in range(len(trainY)):
+    trainingset.append((trainX[i,:,:], trainY[i,:]))
+    
+
+
+trainloader = torch.utils.data.DataLoader(trainingset, batch_size=bsz, shuffle=False)
 
 
 # In[5]:
@@ -99,78 +83,56 @@ testY = Variable(torch.Tensor(np.array(y[train_size:len(y)]))).to(device)
 num_epochs = 2000
 learning_rate = 1e-3
 
-input_size = 17
+input_size = 4
 hidden_size = 256
 num_layers = 1
+seq_length = 4
 num_classes = 1
 
 lstm = LSTM(num_classes, input_size, hidden_size, num_layers, seq_length, fc1_out = 128)
 
-lstm = lstm.to(device)
+
+
 # In[ ]:
 
 
-lstm.train()
+trainX = trainX.unsqueeze(1)
 test_hist = []
-criterion = torch.nn.MSELoss().to(device)    # mean-squared error for regression
+criterion = torch.nn.MSELoss()    # mean-squared error for regression
 optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
 
 for epoch in range(num_epochs):
-
-        outputs = lstm(trainX).to(device)
+    total_loss = 0
+    j = 0  
+    for i, (inputs, targets) in enumerate(trainloader):
+        outputs = lstm(inputs.to(device))
 
         optimizer.zero_grad()
-    
     # obtain the loss function
-        loss = criterion(outputs, trainY)
+        loss = criterion(outputs, targets.to(device))
     
         loss.backward()
     
         optimizer.step()
-        if testX is not None:
-            with torch.no_grad():
-                y_test_pred = lstm(testX).to(device)
-                test_loss = criterion(y_test_pred.float(), testY).to(device)
-                test_hist.append( test_loss.item())
-        if epoch % 5 == 0:
-            print("Epoch: %d, train loss: %1.5f, test loss: %1.5f" % (epoch, loss.item(), test_hist[epoch]))
+        
+        total_loss += loss.item()
+        j +=1
+    if testX is not None:
+        with torch.no_grad():
+            y_test_pred = lstm(testX.to(device))
+            test_loss = criterion(y_test_pred.float(), testY.to(device))
+            test_hist.append( test_loss.item())
+    total_loss /=j
+    if epoch % 2 == 0:
+        print("Epoch: %d, train loss: %1.5f, test loss: %1.5f" % (epoch, total_loss, test_hist[epoch]))
+        torch.save(lstm.state_dict(), f'../Models/midprice-regression-new')
 
-            torch.save(lstm.state_dict(), '../Models/state-midprice-regression')
-# In[ ]:
-
-
-import matplotlib.pyplot as plt
-
-lstm.eval()
-train_predict = lstm(dataX)
-data_predict = train_predict.data.numpy()[train_size+ test_size - 2000:train_size + test_size]
-dataY_plot = dataY.data.numpy()[train_size + test_size -2000:train_size + test_size]
-data_predict = sc_y.inverse_transform(data_predict)
-dataY_plot = sc_y.inverse_transform(dataY_plot)
-
-#plt.axvline(x=train_size, c='r', linestyle='--')
-
-plt.plot(dataY_plot)
-plt.plot(data_predict, c='green')
-plt.suptitle('Time-Series Prediction')
-plt.show()
-
-
-# In[ ]:
-
-
-torch.save(lstm.state_dict(), '../Models/state-midprice-regression')
-
-
-# In[ ]:
-
-
+import pickle
 with open('sc_midprice', 'wb') as f:
-    pickle.dump(sc_y, f)
+    pickle.dump(sc, f)
     
 
 
-# In[ ]:
 
 
 
